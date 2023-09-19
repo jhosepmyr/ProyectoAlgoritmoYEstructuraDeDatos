8f5df03ef59f9140a639546e1ad8e58e49d4003a
Elmer Augusto Riva Rodriguez <u202220829@upc.edu.pe>
2023-09-19 08:56:04 -0500

    comentarios

--- ProyectoAlgoritmoYEstructuraDeDatos/InterfazLogin.h
+++ ProyectoAlgoritmoYEstructuraDeDatos/InterfazLogin.h
@@ -30,12 +30,12 @@ public:
             }
             validarOpcion();
             system("cls");
-            generarInterfazTipoUsuario();
+            generarInterfazTipoUsuario(); //metodo donde esta la llamada a la clase Login
         } while (this->opcion < 1 || this->opcion > 3);
     }
     
     void validarOpcion() {
-        auto opcionValida = [](char op)->bool {return op == '1' || op == '2' || op == '3'; };
+        auto opcionValida = [](char op)->bool {return op == '1' || op == '2' || op == '3'; }; //lambda
         if (!opcionValida(this->opcion) || !opcionValida(this->tipoUsuario)) {
             cout << "Seleccione una opcion valida.\n" << endl;
             this->opcion = 4;
