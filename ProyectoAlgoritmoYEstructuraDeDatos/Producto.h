05a213d2f0c846ebd3c04830175c4a2f1bce5192
hmongus <115027624+hmongus@users.noreply.github.com>
2023-09-18 21:35:27 -0500

    Update Producto.h

--- ProyectoAlgoritmoYEstructuraDeDatos/Producto.h
+++ ProyectoAlgoritmoYEstructuraDeDatos/Producto.h
@@ -6,10 +6,14 @@ class Producto {
 protected:
 	int id;
 	string Nombre;
-	int precio;
+	double precio;
+	string material;
+	string estado;
+	int t_vida;//tiempo de vida 
 public:
-	Producto() :id(0), Nombre(" "), precio(0) {};
-	Producto(int id, string Nombre, int precio) :id(id), Nombre(Nombre), precio(precio) {};
+	Producto() :id(0), Nombre(" "), precio(0), material(" "), estado(" "), t_vida(0) {};
+	Producto(int id, string Nombre, int precio, string material, string estado, int t_vida) :
+		id(id), Nombre(Nombre), precio(precio), material(material), estado(estado), t_vida(t_vida) {};
 	~Producto() {};
 
 	void mostrarDatos() {
@@ -17,11 +21,15 @@ public:
 		cout << "\nID: " << id;
 		cout << "\nNombre: " << Nombre;
 		cout << "\nPrecio: " << precio;
+		cout << "\nMaterial: " << material;
+		cout << "\nEstado: " << estado;
+		cout << "\nTiempo estimado de vida: " << t_vida;
+
 	}
 	void ModificarID(int n) { id = n; }
 	void ModificarNombre(string nm) { Nombre = nm; }
 	void ModificarPrecio(int pr) { precio = pr; }
-
+	
 	int getID() { return id; }
 	string getNombre() { return Nombre; }
 	int getPrecio() { return precio; }
