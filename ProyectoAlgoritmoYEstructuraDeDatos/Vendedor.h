17a4dfd37535a2a345681282c79844df3d23797e
Elmer Augusto Riva Rodriguez <u202220829@upc.edu.pe>
2023-09-21 23:13:06 -0500

    cambios en vendedor

--- ProyectoAlgoritmoYEstructuraDeDatos/Vendedor.h
+++ ProyectoAlgoritmoYEstructuraDeDatos/Vendedor.h
@@ -10,19 +10,25 @@ private:
     Lista<Producto> productosPropios;
     //implementar con el ID Ejem: vendedor10.txt
 public:
-    Vendedor(string _nombre, string _contra) : Usuario(_nombre, _contra){
+    Vendedor(string _nombre, string _contra) : Usuario(_nombre, _contra) {
         this->datosInicioSesion = "DatosSesionVendedores.txt";
-
+        setProductosPropios();// al momento de instanciar se obtiene la lista de productos del vendedor especifico
     }
-    void mostrarMisProductos() {
-        Lista<Producto> auxProductos;
-        Files archivoProductos;
-        auxProductos = archivoProductos.getProductos();
+    void setProductosPropios() { //elimina los productos que no son del vendedor que inica sesion
+        this->productosPropios = this->productos; //primero la lista adquiere todos los productos
+
         string auxID = this->nombre + this->contra;
-        for (int i = 0; i < auxProductos.longitud();i++) {
-            if (auxProductos.obtenerPos(i).getIdentificador() == auxID) {
-                auxProductos.obtenerPos(i).mostrarDatos();
+        for (int i = 0; i < productosPropios.longitud(); i++) {
+            if (productosPropios.obtenerPos(i).getIdentificador() != auxID) { //utiliza el id de producto (que es la username+id)
+                productosPropios.eliminaPos(i);
+                i--;
             }
         }
+    }
+    void mostrarMisProductos() {
+        productosPropios.MostrarDatosPROD();
+    }
+    void modificarProductos() {
+
     }
 };
