2e0df73efef3d935cbbf7f2f3b0e59d669e08628
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2023-09-24 17:26:33 -0500

    xd

--- ProyectoAlgoritmoYEstructuraDeDatos/Lambdas.h
+++ ProyectoAlgoritmoYEstructuraDeDatos/Lambdas.h
@@ -1,5 +1,6 @@
-//Interfaz 
-/*auto opcionValida = [](char op)->bool {return op == '1' || op == '2' || op == '3'; };*/
+//Interfaz -> validarOpcion()
+/*auto opcionValida = 
+[](char op)->bool {return op == '1' || op == '2' || op == '3'; };*/
 
 //Producto -> mostrarDatosLinea()
 /*auto formatLine = [this]() {
