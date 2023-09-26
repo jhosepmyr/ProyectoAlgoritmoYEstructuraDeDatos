1cd14f4630a57b41f7885664b9dfd78546844e9b
Elmer Augusto Riva Rodriguez <u202220829@upc.edu.pe>
2023-09-25 23:19:57 -0500

    changes

--- ProyectoAlgoritmoYEstructuraDeDatos/Recursividad.h
+++ ProyectoAlgoritmoYEstructuraDeDatos/Recursividad.h
@@ -1,1 +1,20 @@
 #pragma once
+
+//En la clase Lista
+/*template <class T>
+T Lista<T>::loopdeBusquedaDATO(Nodo* aux, string ID) {
+    if (aux->elem.getNombre() == ID) {
+        return aux->elem;
+    }
+    else  if (aux != nullptr) return loopdeBusquedaDATO(aux = aux->sig, ID);
+}*/
+
+//En la clase Pila
+/*template<class T>
+void Pila<T>::mostrarDatosPila() {
+	T aux = pop();
+	if (estaVacia()) return;
+	aux.mostrarListaProductos();
+	mostrarDatosPila();
+}*/
+
