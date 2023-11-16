8251e15e791d0549008c97827975bd1f65f03527
Jhosepmyr Orlando Gutierrez Soto <jhosepmyrgutierrezsoto@gmail.com>
2023-11-16 11:40:35 -0500

    arreglado

--- ProyectoAlgoritmoYEstructuraDeDatos/ArbolAVL.h
+++ ProyectoAlgoritmoYEstructuraDeDatos/ArbolAVL.h
@@ -14,14 +14,14 @@ public:
 };
 
 template<class T>
-class Nodo {
+class NodoAVL {
 public:
 	T elemento;
-	Nodo* izq;
-	Nodo* der;
+	NodoAVL* izq;
+	NodoAVL* der;
 	int altura;
 
-	Nodo() {
+	NodoAVL() {
 		izq = nullptr;
 		der = nullptr;
 		altura = 0;
@@ -31,91 +31,91 @@ public:
 template<class T>
 class ArbolAVL {
 private:
-	Nodo<T>* raiz;
+	NodoAVL<T>* raiz;
 	void(*procesar)(T); //puntero a función
 
 	//Operaciones privadas
-	int _altura(Nodo<T>* nodo) {
-		if (nodo == nullptr) return 0;
-		return nodo->altura;
+	int _altura(NodoAVL<T>* NodoAVL) {
+		if (NodoAVL == nullptr) return 0;
+		return NodoAVL->altura;
 	}
 
-	void _rotarDerecha(Nodo<T>*& nodo) {
-		Nodo<T>* p = nodo->izq;
-		nodo->izq = p->der;
-		p->der = nodo;
+	void _rotarDerecha(NodoAVL<T>*& NodoAVL) {
+		NodoAVL<T>* p = NodoAVL->izq;
+		NodoAVL->izq = p->der;
+		p->der = NodoAVL;
 		//Actualizamos la altura
 
-		nodo = p;
+		NodoAVL = p;
 	}
 
-	void _rotarIzquierda(Nodo<T>*& nodo) {
-		Nodo<T>* p = nodo->der;
-		nodo->der = p->izq;
-		p->izq = nodo;
+	void _rotarIzquierda(NodoAVL<T>*& NodoAVL) {
+		NodoAVL<T>* p = NodoAVL->der;
+		NodoAVL->der = p->izq;
+		p->izq = NodoAVL;
 		//Actualizamos la altura
 
-		nodo = p;
+		NodoAVL = p;
 	}
-	void _balanceo(Nodo<T>*& nodo) {
-		int hizq = _altura(nodo->izq);
-		int hder = _altura(nodo->der);
+	void _balanceo(NodoAVL<T>*& NodoAVL) {
+		int hizq = _altura(NodoAVL->izq);
+		int hder = _altura(NodoAVL->der);
 		int fb = hder - hizq;
 
 		if (fb > 1) { //rotar a la izq
-			int hhizq = _altura(nodo->der->izq);
-			int hhder = _altura(nodo->der->der);
+			int hhizq = _altura(NodoAVL->der->izq);
+			int hhder = _altura(NodoAVL->der->der);
 			if (hhizq > hhder) { //verificar si aplica doble rotación
-				_rotarDerecha(nodo->der);
+				_rotarDerecha(NodoAVL->der);
 			}
-			_rotarIzquierda(nodo);
+			_rotarIzquierda(NodoAVL);
 		}
 		if (fb < -1) { //rotar a la der
-			int hhizq = _altura(nodo->izq->izq);
-			int hhder = _altura(nodo->izq->der);
+			int hhizq = _altura(NodoAVL->izq->izq);
+			int hhder = _altura(NodoAVL->izq->der);
 			if (hhizq < hhder) {//verificar si aplica doble rotación 
-				_rotarIzquierda(nodo->izq);
+				_rotarIzquierda(NodoAVL->izq);
 			}
-			_rotarDerecha(nodo);
+			_rotarDerecha(NodoAVL);
 		}
-		//Actualizar la altura del nodo raiz
-		hizq = _altura(nodo->izq);
-		hder = _altura(nodo->der);
-		nodo->altura = 1 + ((hizq > hder) ? hizq : hder);
+		//Actualizar la altura del NodoAVL raiz
+		hizq = _altura(NodoAVL->izq);
+		hder = _altura(NodoAVL->der);
+		NodoAVL->altura = 1 + ((hizq > hder) ? hizq : hder);
 	}
 
-	bool _insertar(Nodo<T>*& nodo, T e) {
+	bool _insertar(NodoAVL<T>*& NodoAVL, T e) {
 
-		if (nodo == nullptr) {
+		if (NodoAVL == nullptr) {
 			//Nuevo elemento
-			nodo = new Nodo<T>();
-			nodo->elemento = e;
+			NodoAVL = new NodoAVL<T>();
+			NodoAVL->elemento = e;
 			return true;
 		}
-		else if (e == nodo->elemento)
+		else if (e == NodoAVL->elemento)
 			return false;
-		else if (e < nodo->elemento) {
-			_insertar(nodo->izq, e);
+		else if (e < NodoAVL->elemento) {
+			_insertar(NodoAVL->izq, e);
 		}
-		else if (e > nodo->elemento) {
-			_insertar(nodo->der, e);
+		else if (e > NodoAVL->elemento) {
+			_insertar(NodoAVL->der, e);
 		}
-		_balanceo(nodo);
+		_balanceo(NodoAVL);
 		return true;
 	}
 
-	void _inOrden(Nodo<T>* nodo) {
-		if (nodo == nullptr) return;
-		_inOrden(nodo->izq);
-		procesar(nodo->elemento);
-		_inOrden(nodo->der);
+	void _inOrden(NodoAVL<T>* NodoAVL) {
+		if (NodoAVL == nullptr) return;
+		_inOrden(NodoAVL->izq);
+		procesar(NodoAVL->elemento);
+		_inOrden(NodoAVL->der);
 	}
 
-	void _inOrdenH(Nodo<T>* nodo) {
-		if (nodo == nullptr) return;
-		_inOrdenH(nodo->izq);
-		procesar(nodo->altura);
-		_inOrdenH(nodo->der);
+	void _inOrdenH(NodoAVL<T>* NodoAVL) {
+		if (NodoAVL == nullptr) return;
+		_inOrdenH(NodoAVL->izq);
+		procesar(NodoAVL->altura);
+		_inOrdenH(NodoAVL->der);
 	}
 
 public:
