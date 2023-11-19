19b3d613e65d82017a910f1f313248c37e3bce77
Elmer Augusto Riva Rodriguez <u202220829@upc.edu.pe>
2023-11-19 16:19:07 -0500

    coco

--- ProyectoAlgoritmoYEstructuraDeDatos/InterfazInicio.h
+++ ProyectoAlgoritmoYEstructuraDeDatos/InterfazInicio.h
@@ -1,5 +1,7 @@
-#pragma once
+﻿#pragma once
 #include "InterfazLogin.h"
+#include <thread>
+#include <chrono>
 using namespace System;
 using namespace std;
 
@@ -13,7 +15,7 @@ public:
     void ventanaInicio() {
         Console::SetCursorPosition(0, 8);
         Console::ForegroundColor = ConsoleColor::White;
- 
+
         cout << R"(
          _______  __    __  .______       __    __    
         /  _____||  |  |  | |   _  \     |  |  |  |   
@@ -32,6 +34,30 @@ public:
         Console::SetCursorPosition(14, 22);
         cout << "Presiona ENTER para continuar";
         cin.get();
+        system("cls");
+        Console::SetCursorPosition(6, 16);
+        int duracion = 5000;
+        int porcentajeTotal = 40;
+        int intervaloActualizacion = 100;
+        int transcurrido = 0;
+
+        while (transcurrido < duracion) {
+            int porcentaje = (transcurrido * 100) / duracion;
+            int bloquesLlenos = (porcentaje * porcentajeTotal) / 100;
+
+            cout << "\r\t[";
+            for (int i = 0; i < bloquesLlenos; ++i) {
+                cout << char(219);
+            }
+            for (int i = bloquesLlenos; i < porcentajeTotal; ++i) {
+                cout << char(176);
+            }
+            cout << "] " << porcentaje << "%";
+            cout.flush();
+
+            this_thread::sleep_for(chrono::milliseconds(intervaloActualizacion));
+            transcurrido += intervaloActualizacion;
+        }
         system("cls");
     }
     void iniciarLogin() {
